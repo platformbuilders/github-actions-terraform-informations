8721bf99a6e4d6945ac1d2ef5cf2a2225bc71ae2
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2022-03-29 22:29:24 -0300

    Changes in github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 diff=$(git diff-tree --no-commit-id --name-status -r ${GITHUB_SHA::7} --diff-filter=ACM | xargs)
 
-action=$(echo $teste | awk '{print $1}')
-file=$(echo $teste | awk '{print $2}')
+action=$(echo $diff | awk '{print $1}')
+file=$(echo $diff | awk '{print $2}')
 env=$(echo $file | sed 's;/; ;g' | awk '{print $1}')
 region=$(echo $file | sed 's;/; ;g' | awk '{print $2}')
 service=$(echo $file | sed 's;/; ;g' | awk '{print $3}')
