426af5b704a19cdbb339d135322099cc4e1e9d9d
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2022-03-29 22:37:24 -0300

    Changes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 diff=$(git diff-tree --no-commit-id --name-status -r ${GITHUB_SHA::7} --diff-filter=ACM | xargs)
 
-action=$(echo $diff | awk '{print $1}')
-file=$(echo $diff | awk '{print $2}')
-env=$(echo $file | sed 's;/; ;g' | awk '{print $1}')
-region=$(echo $file | sed 's;/; ;g' | awk '{print $2}')
-service=$(echo $file | sed 's;/; ;g' | awk '{print $3}')
-resource=$(echo $file | sed 's;/; ;g' | awk '{print $4}')
+echo action=$(echo $diff | awk '{print $1}') >> $GITHUB_ENV
+echo file=$(echo $diff | awk '{print $2}') >> $GITHUB_ENV
+echo env=$(echo $file | sed 's;/; ;g' | awk '{print $1}') >> $GITHUB_ENV
+echo region=$(echo $file | sed 's;/; ;g' | awk '{print $2}') >> $GITHUB_ENV
+echo service=$(echo $file | sed 's;/; ;g' | awk '{print $3}') >> $GITHUB_ENV
+echo resource=$(echo $file | sed 's;/; ;g' | awk '{print $4}') >> $GITHUB_ENV
 
 echo "Git action kind: " $action
 echo "File path: " $file
