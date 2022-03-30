58129bf4c20b339df0e7f397fca08bbe9510dd96
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2022-03-30 17:31:23 -0300

    Changes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ json=$(while IFS= read -r line; do
     jq -n --arg env "$env" --arg path "$line" '{env: $env, path: $path}'
 done < "path_file_sorted.txt" | jq -n '.items |= [inputs]')
 
+json=$(echo $json | sed 's/"/\\"/g' | sed -e 's/ //g' )
 echo json=$json >> $GITHUB_ENV
 echo $json | jq '.'
 rm -f *.txt
